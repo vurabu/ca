5c41139b7a65309bbc3d6c936bae741d6b05b449
cirno <vurabu@gmail.com>
2012-02-02 13:11:33 +0600

    :<

--- visualizer.cpp
+++ visualizer.cpp
@@ -1,20 +1,5 @@
 #include "visualizer.h"
 
-string name_suffix;
-
-void saveVisualizationConfig(const string& new_name_suffix) {
-    name_suffix = new_name_suffix;
-    ofstream config_file("last_run.cfg");
-    if(!config_file.is_open()) {
-        cerr << "Can't open last_run.cfg" << endl;
-        return ;
-    }
-    config_file << "NAME_SUFFIX=\"" << name_suffix << "\"" << endl;
-    config_file << "FIELD_HEIGHT=" << field_height << endl;
-    config_file << "FIELD_WIDTH=" << field_width << endl;
-    config_file.close();
-}
-
 bool addCell(int row, int col, double *sum) {
     if(0 < row && row < field_height - 1 && 0 < col && col < field_width - 1) {
         (*sum) += cur_iteration[row * field_width + col].getWeight();
@@ -29,7 +14,7 @@ void genBinary(size_t cur_it) {
     char filename[1<<5];
     FILE *cur_it_file;
 
-    sprintf(filename, "%06lu%s.bin", cur_it, name_suffix.c_str());
+    sprintf(filename, "%06lu.bin", cur_it);
     assert(cur_it_file = fopen(filename, "w"));
     
     int allsum = 0;
